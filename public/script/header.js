860c48d63e6d21c0b46485a54751632f7ee15d67
end <quantumhelix@logandigital.com>
2024-05-23 16:06:07 -0700

    updated header lil bit

--- public/script/header.js
+++ public/script/header.js
@@ -3,15 +3,15 @@ const headerHTML = `
 <a href="/index.html" class="logo">
   <img src="/resources/images/aroace_dragon.PNG" style="width:63%;">
 </a>
-<h3 class="alternate" style="float:left;padding:15px 0 15px 25px;font-size:25px;">end.site</h3>
-<div class="clickyButtons"><button type="button" onclick="toggleButtons()">Toggle Buttons</button> <button type="button" onclick="toggleAnimations()">Toggle Animations</button></div>
-<div class="header-right">
-  <a class="active" href="/pages/about/index.html">About</a>
-  <a class="active" href="/pages/blog/index.html">Blog</a>
-  <a class="active" href="/pages/projects/index.html">Creations</a>
-  <a class="active" href="/pages/socials/index.html">Socials</a>
-  <a class="active" href="/pages/links/index.html">Linkies</a>
-</div>`;
+<h3 class="alternate" style="float:left;padding:15px 0 15px 25px;font-size:25px;">end.site</h3><br>
+<span class="clickyButtons"><button type="button" onclick="toggleButtons()">Toggle Buttons</button> <button type="button" onclick="toggleAnimations()">Toggle Animations</button></span>
+<span class="header-right">
+  <a href="/pages/about/index.html">About</a>
+  <a href="/pages/blog/index.html">Blog</a>
+  <a href="/pages/projects/index.html">Creations</a>
+  <a href="/pages/socials/index.html">Socials</a>
+  <a href="/pages/links/index.html">Linkies</a>
+</span>`;
 
 if (document.getElementById("header")) {
   document.getElementById("header").innerHTML = headerHTML;
