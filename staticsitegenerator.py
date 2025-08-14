54f44167bed28fea32b0a5985933eecde1877e65
EndDragon438 <quantumhelix@logandigital.com>
2025-08-13 23:41:02 -0700

    cleaned up pages in prep for rework

--- staticsitegenerator.py
+++ staticsitegenerator.py
@@ -15,24 +15,61 @@ Distributed under the terms of the GPLv3
 https://www.gnu.org/licenses/gpl-3.0.en.html
 '''
 
+idea_count = 69
+last_updated = "LAST MODIFIED: AUGUST 13, 2025"
+link_count = 289
+
+
+
 # go through all /private/ files/folders and call the requisite functions for all of them
-def generate:
+def generate():
+    # loop over all pages (recursively) and call:
+    set_head()
+
+    # loop over all pages (recursively) and call:
+    set_header()
+
+    # generate all tag pages
+    generate_tags()
+
+    # generate all posts from /snippets/data.json
+    generate_posts()
+
+    # populate tag pages with newly generated posts
+    populate_tags()
+
+
+# sets the <end-head> tag to the standard header
+def set_head():
+    # make sure to not overwrite the default <head>, those may be used for unique pages
     pass
 
 # set the <end-header> tag of a page to the static header
-def set_header:
+def set_header():
     # make sure to not replace other <header> tags, because I would use those for unique headers on some pages
     pass
 
 # generate an HTML page from a post JSON file
-def generate_post:
+def generate_post(path):
+    pass
+
+# generate all posts /snippets/data.json
+def generate_posts():
+    # grab posts from data.json
+    posts = {}
+
+    # loop over posts creating pages
+    for post in posts:
+        generate_post("/snippets/posts/" + post)
+
+# generates pages for each tag in use from /snippets/data.json
+def generate_tags():
     pass
 
 # populates tag pages with related posts
-def populate_tags:
+def populate_tags():
+    # TODO: /pages/blog/index.html, /pages/projects/tagged.html, /pages/project/tags/*.html
     pass
 
-# sets the <end-head> tag to the standard header
-def set_head:
-    # make sure to not overwrite the default <head>, those may be used for unique pages
-    pass
+
+generate()
